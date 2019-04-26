1f7d672dcffbe34e0b173c0ff22adf17ecde7996
vuhoi <toilahoi007@gmail.com>
2019-04-27 01:04:07 +0700

    initial

---
+++ src/app/gwebsite/gwebsite-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuClientComponent } from '@app/gwebsite/menu-client/menu-client.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([
+            {
+                path: '',
+                children: [
+                    {
+                        path: 'menu-client', component: MenuClientComponent,
+                        data: { permission: 'Pages.Administration.MenuClient' }
+                    },
+                ]
+            },
+          
+           
+        ])
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class GWebsiteRoutingModule { }
---
+++ src/app/gwebsite/gwebsite.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppCommonModule } from '@app/shared/common/app-common.module';
+import { UtilsModule } from '@shared/utils/utils.module';
+import { FileUploadModule } from 'ng2-file-upload';
+import { ModalModule, PopoverModule, TabsModule, TooltipModule } from 'ngx-bootstrap';
+import { AutoCompleteModule, EditorModule, FileUploadModule as PrimeNgFileUploadModule, InputMaskModule, PaginatorModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
+import { GWebsiteRoutingModule } from './gwebsite-routing.module';
+
+import { MenuClientComponent, CreateOrEditMenuClientModalComponent } from './index';
+import { DemoModelServiceProxy } from '@shared/service-proxies/service-proxies';
+import { PurchaseComponent } from './purchase/purchase/purchase.component';
+
+@NgModule({
+    imports: [
+        FormsModule,
+        CommonModule,
+        FileUploadModule,
+        ModalModule.forRoot(),
+        TabsModule.forRoot(),
+        TooltipModule.forRoot(),
+        PopoverModule.forRoot(),
+        GWebsiteRoutingModule,
+        UtilsModule,
+        AppCommonModule,
+        TableModule,
+        PaginatorModule,
+        PrimeNgFileUploadModule,
+        AutoCompleteModule,
+        EditorModule,
+        InputMaskModule
+    ],
+    declarations: [
+        MenuClientComponent, CreateOrEditMenuClientModalComponent,
+        PurchaseComponent
+    ],
+    providers: [
+        DemoModelServiceProxy
+    ]
+})
+export class GWebsiteModule { }
