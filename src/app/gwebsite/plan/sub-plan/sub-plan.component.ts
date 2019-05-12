b7a52aa5e4ca4e14c8b58740c320a09f87a23aa1
lut2410 <wakawaka2610@gmail.com>
2019-05-12 17:56:17 +0700

    fix sub plan

--- src/app/gwebsite/plan/sub-plan/sub-plan.component.ts
+++ src/app/gwebsite/plan/sub-plan/sub-plan.component.ts
@@ -28,9 +28,11 @@ export class SubPlanComponent extends AppComponentBase implements AfterViewInit,
    * tạo các biến dể filters
    */
   public filterText: string;
+  public productCode: string;
   public planId = 100;
   public yearImplement = 2019; //get by plan id
   public YearImplementList = [this.yearImplement];
+  public approvalStatusEnum = ApprovalStatusEnum;
   public approvalStatus = ApprovalStatusEnum.AwaitingApproval;
   public ApprovalStatusList = [
     {
@@ -42,6 +44,139 @@ export class SubPlanComponent extends AppComponentBase implements AfterViewInit,
   public totalPrice = 56000000;
   public implementPrice = 30000000;
   public residualPrice = +this.totalPrice - +this.implementPrice;
+
+  public datas = [
+    {
+      productCode: 'A001',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 10,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'A002',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 11,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'B001',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 12,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'B002',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 13,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'C001',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 14,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'C002',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 15,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'D001',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 16,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'D002',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 17,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'E001',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 18,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    },
+    {
+      productCode: 'E002',
+      productName: 'Computer',
+      calculationUnit: 'Int',
+      quantity: 19,
+      totalPrice: 1000000,
+      scheduleMonth: 1,
+      implementQuantity: 7,
+      implementPrice: 700000,
+      implementMonth: 2,
+      pesidualQuantity: 3,
+      pesidualPrice: 300000
+    }
+  ];
 
   constructor(
     injector: Injector,
@@ -83,17 +218,26 @@ export class SubPlanComponent extends AppComponentBase implements AfterViewInit,
      * Sử dụng _apiService để call các api của backend
      */
 
-    // tiennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-    this._apiService.get('api/MenuClient/GetMenuClientsByFilter',
-      [{ fieldName: 'Name', value: this.filterText }],
-      this.primengTableHelper.getSorting(this.dataTable),
-      this.primengTableHelper.getMaxResultCount(this.paginator, event),
-      this.primengTableHelper.getSkipCount(this.paginator, event),
-    ).subscribe(result => {
-      this.primengTableHelper.totalRecordsCount = result.totalCount;
-      this.primengTableHelper.records = result.items;
-      this.primengTableHelper.hideLoadingIndicator();
+    // this._apiService.get('api/MenuClient/GetMenuClientsByFilter',
+    //   [{ fieldName: 'Name', value: this.filterText }],
+    //   this.primengTableHelper.getSorting(this.dataTable),
+    //   this.primengTableHelper.getMaxResultCount(this.paginator, event),
+    //   this.primengTableHelper.getSkipCount(this.paginator, event),
+    // ).subscribe(result => {
+    //   this.primengTableHelper.totalRecordsCount = result.totalCount;
+    //   this.primengTableHelper.records = result.items;
+    //   this.primengTableHelper.hideLoadingIndicator();
+    // });
+
+    this.primengTableHelper.totalRecordsCount = 14;
+    this.primengTableHelper.records = this.datas;
+
+    this.primengTableHelper.records.forEach((row: any, i: number) => {
+      row.isEdit = false;
+      row.quantityEdit = 0;
     });
+
+    this.primengTableHelper.hideLoadingIndicator();
   }
 
   init(): void {
@@ -117,20 +261,39 @@ export class SubPlanComponent extends AppComponentBase implements AfterViewInit,
     return abp.utils.truncateStringWithPostfix(text, 32, '...');
   }
 
-  public searchSubPlan(): void {
-
-  }
   public approvedPlan(): void {
     // this.planId: approved theo planId
-    if (this.approvalStatus === 2) {
+    if (this.approvalStatus === ApprovalStatusEnum.AwaitingApproval) {
 
     }
     //call api approved plan
     //if approved successful
     if (1) {
-      this.approvalStatus = 1;
+      this.approvalStatus = ApprovalStatusEnum.Approved;
       this.ApprovalStatusList[0].id = 1;
       this.ApprovalStatusList[0].name = 'Approved';
     }
+  }
+  public searchProductCode(): void {
+    //search by this.productCode
+  }
+
+  public activeEditRow(row: any): void {
+    row.quantityEdit = row.quantity;
+    row.isEdit = true;
+  }
+
+  public saveEditRow(row: any): void {
+    //call api save
+    //save row.quantityEdit to quantity of row.productCode in this.planId
+    console.log(row.quantityEdit);
+
+    row.quantity = row.quantityEdit;
+    //done save
+    row.isEdit = false;
+  }
+
+  public cancelEditRow(row: any): void {
+    row.isEdit = false;
   }
 }
